46f3339260c00d9688ec9b53149fb4b28443b5a5
SheaParrott <alexandergotthis@gmail.com>
2019-04-14 19:59:57 -0400

    button now toggles gray class that is bound to h1 on home screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  h1Style: boolean = false
   constructor() {}
 
   ngOnInit() {}
   firstClick = () => {
-    console.log('clicked')
+    this.h1Style = !this.h1Style
   }
 }
