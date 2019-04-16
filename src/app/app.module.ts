10ccb63a62052bf186fcf7f24a70d9a7056ffc55
SheaParrott <alexandergotthis@gmail.com>
2019-04-15 23:37:39 -0400

    form with validations mostly set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@Angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -17,7 +18,12 @@ import { ContactComponent } from './contact/contact.component'
     AboutComponent,
     ContactComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
